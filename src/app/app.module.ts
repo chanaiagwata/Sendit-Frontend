47833ba0c7515ac1e644a5c7d6461fc749659477
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-07-06 09:37:03 +0300

    ft:create profile page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TrackingComponent } from './components/tracking/tracking.component';
 import { ServicesComponent } from './components/services/services.component';
 import { PricingComponent } from './components/pricing/pricing.component';
 import { MapComponent } from './components/map/map.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { MapComponent } from './components/map/map.component';
     TrackingComponent,
     ServicesComponent,
     PricingComponent,
-    MapComponent
+    MapComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
